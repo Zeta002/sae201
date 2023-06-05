0525bdf1267397a761ad5403b4880735fb25bfc6
Angelo Portelli <115974047+AngeloPORTELLIP22008750@users.noreply.github.com>
2023-06-05 16:58:08 +0200

    Minor commit
    Transfert du code sur un PC fixe via GitHub pour continuer le travail depuis chez moi

--- src/main/java/com/example/sae201/appController.java
+++ src/main/java/com/example/sae201/appController.java
@@ -1,9 +1,12 @@
 package com.example.sae201;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class appController {
+    @FXML
+    private Button btnMenuDeroulant;
 
 }
 
